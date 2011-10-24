f06cc10e1d5167fde94345c430e58d97096ee384
torstenchr <github.com@bondechristiansen.dk>
2011-10-24 14:18:42 +0000

    * Added filetypes association for Windows in Settings->General

--- settings/settings_general_frame.pas
+++ settings/settings_general_frame.pas
@@ -6,21 +6,26 @@ interface
 
 uses
   Classes, SysUtils, FileUtil, Forms, Controls, StdCtrls, Dialogs, EditBtn,
-  settings2_interface, settings2_var;
+  ExtCtrls, settings2_interface, settings2_var;
 
 type
 
   { TSettings_GeneralFrame }
 
   TSettings_GeneralFrame = class(TFrame, ISettingsFrame)
+    UnAssociateBtn: TButton;
+    AssociateBtn: TButton;
     DefaultPasteCombo: TComboBox;
     DefaultSaveTypeComboBox: TComboBox;
     Label1: TLabel;
     Label18: TLabel;
+    AssociateLabel: TLabel;
     MultipleInstanceChkbox: TCheckBox;
     SaveWindowPositionsChkBox: TCheckBox;
     ShowWelcomeChkBox: TCheckBox;
     ShowWorkToolBarChkBox: TCheckBox;
+    procedure AssociateBtnClick(Sender: TObject);
+    procedure UnAssociateBtnClick(Sender: TObject);
   private
     { private declarations }
     FData: PManagerSettings;
@@ -36,9 +41,77 @@ implementation
 {$R *.lfm}
 
 uses
-  settings2, epimiscutils,strutils, main;
+  settings2, epimiscutils,strutils, main
+  {$IFDEF WINDOWS}
+  ,registry
+  {$ENDIF};
 
 { TSettings_GeneralFrame }
+
+procedure TSettings_GeneralFrame.AssociateBtnClick(Sender: TObject);
+{$IFDEF WINDOWS}
+var
+  Reg: TRegistry;
+const
+  ExtA = '.epx';
+  ExtB = '.epz';
+  FileType = 'EpiDataProjectFile';
+begin
+  Reg:=TRegistry.Create(KEY_WRITE);
+  try
+    Reg.RootKey := HKEY_CLASSES_ROOT;
+    // File Extesion association - associate with "FileType" description!
+    Reg.OpenKey(ExtA, True);
+    Reg.WriteString('', FileType);
+    Reg.CloseKey;
+    Reg.OpenKey(ExtB, True);
+    Reg.WriteString('', FileType);
+    Reg.CloseKey;
+
+    // File Type registration - set programm to execute, etc.
+    Reg.OpenKey(FileType, True);
+    Reg.WriteString('', 'EpiData Project File');
+    Reg.CloseKey;
+    // - icon
+    Reg.OpenKey(Filetype + '\DefaultIcon', True);
+    Reg.WriteString('', Application.ExeName + ',0');
+    reg.CloseKey;
+    // - executable.
+    Reg.OpenKey(Filetype + '\Shell\Open\Command', True);
+    Reg.Writestring('','"' + Application.ExeName + '" "%1"');
+    Reg.CloseKey;
+  finally
+    Reg.Free;
+  end;
+{$ELSE}
+begin
+{$ENDIF}
+end;
+
+procedure TSettings_GeneralFrame.UnAssociateBtnClick(Sender: TObject);
+{$IFDEF WINDOWS}
+var
+  Reg: TRegistry;
+const
+  ExtA = '.epx';
+  ExtB = '.epz';
+  FileType = 'EpiDataProjectFile';
+begin
+  Reg:=TRegistry.Create(KEY_WRITE);
+  try
+    Reg.RootKey := HKEY_CLASSES_ROOT;
+    Reg.DeleteKey(ExtA);
+    Reg.DeleteKey(ExtB);
+    Reg.DeleteKey(FileType);
+    Reg.CloseKey;
+  finally
+    Reg.Free;
+  end;
+{$ELSE}
+begin
+{$ENDIF}
+end;
+
 
 constructor TSettings_GeneralFrame.Create(TheOwner: TComponent);
 var
@@ -49,6 +122,11 @@ begin
   DefaultSaveTypeComboBox.AddItem(Copy2SymbDel(S, '|'), nil); Copy2SymbDel(S, '|');
   DefaultSaveTypeComboBox.AddItem(Copy2SymbDel(S, '|'), nil); Copy2SymbDel(S, '|');
   DefaultSaveTypeComboBox.ItemIndex := 0;
+  {$IFNDEF WINDOWS}
+  AssociateBtn.Visible := false;
+  UnAssociateBtn.Visible := false;
+  AssociateLabel.Visible := false;
+  {$ENDIF}
 end;
 
 procedure TSettings_GeneralFrame.SetSettings(Data: PManagerSettings);
