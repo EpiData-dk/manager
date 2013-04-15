993dccd2ae56a51d3a744ffd2e91e8dc394b0787
torstenchr <github.com@bondechristiansen.dk>
2013-04-15 08:24:34 +0000

    * Removed use of HTMLComp -> launch external browser instead. * Removed "Combined" report. * Updated Codebook report * Updated Extended fieldlist report

--- reports/report_codebook.pas
+++ reports/report_codebook.pas
@@ -21,8 +21,7 @@ type
 implementation
 
 uses
-  epireport_base, epireport_report_fieldlist, epireport_report_fieldinfo,
-  epireport_report_valuelabelsetlist;
+  epireport_base, epireport_report_fieldlist, epireport_report_fieldinfo;
 
 
 resourcestring
@@ -59,16 +58,7 @@ begin
     TEpiReportFieldInfo(R).Field := Doc.DataFiles[0].Fields[i];
     R.RunReport;
     R.Free;
-    Generator.Line('');
-  end;
-
-  for i := 0 to Doc.ValueLabelSets.Count - 1 do
-  begin
-    R := TEpiReportValueLabelSetList.Create(Generator);
-    TEpiReportValueLabelSetList(R).ValueLabelSet := Doc.ValueLabelSets[i];
-    R.RunReport;
-    R.Free;
-    if not (i = Doc.ValueLabelSets.Count - 1) then
+    if not (i = Doc.DataFiles[0].Fields.Count - 1) then
       Generator.Line('');
   end;
 end;
