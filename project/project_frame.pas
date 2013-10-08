36f00d6a6613a39116b69ea5cff754e7c562d370
torstenchr <github.com@bondechristiansen.dk>
2013-10-08 08:21:00 +0000

    * Merged with trunk r. 708: * - Updated Mainform caption after project title change.

--- project/project_frame.pas
+++ project/project_frame.pas
@@ -286,6 +286,7 @@ begin
   F := TStudyUnitForm.Create(Self, EpiDocument.Study);
   F.ShowModal;
   F.Free;
+  UpdateCaption;
 end;
 
 procedure TProjectFrame.ValueLabelEditorActionExecute(Sender: TObject);
