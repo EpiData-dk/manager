22f73ae5c474e98e140e2b053b01112cc0a32761
torstenchr <github.com@bondechristiansen.dk>
2014-01-14 07:51:51 +0000

    * Report: Changed codebook and fieldlist to use list of controls instead of just fields.

--- reports/report_fieldlist.pas
+++ reports/report_fieldlist.pas
@@ -38,14 +38,14 @@ end;
 procedure TReportFieldLists.DoDocumentReport(const Doc: TEpiDocument;
   const FileName: string; const Index: Integer);
 var
-  R: TEpiReportFieldList;
+  R: TEpiReportControlList;
 begin
   inherited DoDocumentReport(Doc, FileName, Index);
 
-  R := TEpiReportFieldList.Create(Generator);
-  R.Fields := Doc.DataFiles[0].Fields;
+  R := TEpiReportControlList.Create(Generator);
+  R.ControlItems := Doc.DataFiles[0].ControlItems;
   R.ExtendedList := false;
-  R.SortType := stEntryFlow;
+//  R.SortType := stEntryFlow;
   R.RunReport;
   R.Free;
 end;
