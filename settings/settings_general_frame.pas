306023e1826b812313b4bb70b8fab3a59a4c4ec1
torstenchr <github.com@bondechristiansen.dk>
2015-02-04 12:31:39 +0000

    * Changed implementation for File assocation under windows.

--- settings/settings_general_frame.pas
+++ settings/settings_general_frame.pas
@@ -45,50 +45,17 @@ implementation
 {$R *.lfm}
 
 uses
-  settings2, epimiscutils,strutils, main
+  settings2, epimiscutils, strutils
   {$IFDEF WINDOWS}
-  ,registry
+  ,registry, ufileassociation
   {$ENDIF};
 
 { TSettings_GeneralFrame }
 
 procedure TSettings_GeneralFrame.AssociateBtnClick(Sender: TObject);
+begin
 {$IFDEF WINDOWS}
-var
-  Reg: TRegistry;
-const
-  ExtA = '.epx';
-  ExtB = '.epz';
-  FileType = 'EpiDataProjectFile';
-begin
-  Reg:=TRegistry.Create(KEY_WRITE);
-  try
-    Reg.RootKey := HKEY_CLASSES_ROOT;
-    // File Extesion association - associate with "FileType" description!
-    Reg.OpenKey(ExtA, True);
-    Reg.WriteString('', FileType);
-    Reg.CloseKey;
-    Reg.OpenKey(ExtB, True);
-    Reg.WriteString('', FileType);
-    Reg.CloseKey;
-
-    // File Type registration - set programm to execute, etc.
-    Reg.OpenKey(FileType, True);
-    Reg.WriteString('', 'EpiData Project File');
-    Reg.CloseKey;
-    // - icon
-    Reg.OpenKey(Filetype + '\DefaultIcon', True);
-    Reg.WriteString('', Application.ExeName + ',0');
-    reg.CloseKey;
-    // - executable.
-    Reg.OpenKey(Filetype + '\Shell\Open\Command', True);
-    Reg.Writestring('','"' + Application.ExeName + '" "%1"');
-    Reg.CloseKey;
-  finally
-    Reg.Free;
-  end;
-{$ELSE}
-begin
+  AssociateFiles('EpiData Manager', 'Manager Tool', Application.ExeName);
 {$ENDIF}
 end;
 
@@ -98,29 +65,11 @@ begin
 end;
 
 procedure TSettings_GeneralFrame.UnAssociateBtnClick(Sender: TObject);
+begin
 {$IFDEF WINDOWS}
-var
-  Reg: TRegistry;
-const
-  ExtA = '.epx';
-  ExtB = '.epz';
-  FileType = 'EpiDataProjectFile';
-begin
-  Reg:=TRegistry.Create(KEY_WRITE);
-  try
-    Reg.RootKey := HKEY_CLASSES_ROOT;
-    Reg.DeleteKey(ExtA);
-    Reg.DeleteKey(ExtB);
-    Reg.DeleteKey(FileType);
-    Reg.CloseKey;
-  finally
-    Reg.Free;
-  end;
-{$ELSE}
-begin
+  UnAssociateFiles('EpiData Manager', 'Manager Tool', Application.ExeName);
 {$ENDIF}
 end;
-
 
 constructor TSettings_GeneralFrame.Create(TheOwner: TComponent);
 var
