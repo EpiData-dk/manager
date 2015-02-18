481ed938772dd42a115d4eaf5ba9e2a4cecdeef5
torstenchr <github.com@bondechristiansen.dk>
2015-02-18 14:39:29 +0000

    * Implemented initial group form workings. * Implemented initial drag/drop feature user grid -> user-in-group grid.

--- admin/admin_form.pas
+++ admin/admin_form.pas
@@ -60,12 +60,27 @@ type
     procedure NewGroupActionExecute(Sender: TObject);
     procedure NewUserActionExecute(Sender: TObject);
     procedure RemoveUserFromGroupActionExecute(Sender: TObject);
+    procedure UserGridStartDrag(Sender: TObject; var DragObject: TDragObject);
+    procedure UserGroupGridDragDrop(Sender, Source: TObject; X, Y: Integer);
+    procedure UserGroupGridDragOver(Sender, Source: TObject; X, Y: Integer;
+      State: TDragState; var Accept: Boolean);
   private
     { private declarations }
     FAdmin: TEpiAdmin;
     procedure FillGrids;
     procedure FillUserGrid;
+    procedure FillGroupGrid;
+
+  { User methods }
+  private
+    function UserFromGrid: TEpiUser;
     function ShowUserForm(Const User: TEpiUser): TModalResult;
+
+  { Group methods }
+  private
+    function GroupFromGrid: TEpiGroup;
+    function ShowGroupForm(Const Group: TEpiGroup): TModalResult;
+
   public
     { public declarations }
     constructor Create(TheOwner: TComponent; AAdmin: TEpiAdmin);
@@ -77,7 +92,43 @@ implementation
 {$R *.lfm}
 
 uses
-  admin_user_form, admin_group_form;
+  admin_user_form, admin_group_form, types;
+
+type
+
+  { TImageDragObject }
+
+  TImageDragObject = class(TDragControlObject)
+  private
+    FDragImages: TDragImageList;
+  protected
+    function GetDragImages: TDragImageList; override;
+  public
+    constructor Create(AControl: TControl); override;
+    destructor Destroy; override;
+  end;
+
+{ TImageDragObject }
+
+function TImageDragObject.GetDragImages: TDragImageList;
+begin
+  Result := FDragImages;
+end;
+
+constructor TImageDragObject.Create(AControl: TControl);
+var
+  Bitmap: TBitmap;
+begin
+  inherited Create(AControl);
+  FDragImages := TDragImageList.Create(AControl);
+  AlwaysShowDragImages := True;
+end;
+
+destructor TImageDragObject.Destroy;
+begin
+  FDragImages.Free;
+  inherited Destroy;
+end;
 
 { TAdminForm }
 
@@ -97,23 +148,71 @@ begin
   //
 end;
 
-procedure TAdminForm.FillGrids;
+procedure TAdminForm.UserGridStartDrag(Sender: TObject;
+  var DragObject: TDragObject);
 var
-  Idx: Integer;
-  Group: TEpiGroup;
+  CR: TRect;
+  W: Integer;
+  H: Integer;
+  Bitmap: TBitmap;
+  R: TRect;
+  T: LongInt;
+  B: LongInt;
+begin
+  DragObject := TImageDragObject.Create(Sender as TControl);
+  with TImageDragObject(DragObject) do
+  begin
+    T := UserGrid.Selection.Top;
+    B := UserGrid.Selection.Bottom;
+
+    CR := UserGrid.CellRect(0, T);
+    W := CR.Right - CR.Left;
+
+    CR.Bottom := UserGrid.CellRect(0, B).Bottom;
+    H := CR.Bottom - CR.Top;
+
+    FDragImages.Width := W;
+    FDragImages.Height := H;
+    FDragImages.DragHotspot := Point(W, H);
+
+    R := Rect(0, 0, W, H);
+
+    Bitmap := TBitmap.Create;
+    Bitmap.Width := W;
+    Bitmap.Height := H;
+    Bitmap.Canvas.CopyRect(R, UserGrid.Canvas, CR);
+    FDragImages.Add(Bitmap, nil);
+    Bitmap.Free;
+
+    CR := UserGrid.CellRect(0,2);
+    Bitmap := TBitmap.Create;
+    Bitmap.Width := W;
+    Bitmap.Height := H;
+    Bitmap.Canvas.CopyRect(R, UserGrid.Canvas, CR);
+    FDragImages.Add(Bitmap, nil);
+    Bitmap.Free;
+  end;
+end;
+
+procedure TAdminForm.UserGroupGridDragDrop(Sender, Source: TObject; X,
+  Y: Integer);
+begin
+  //
+end;
+
+procedure TAdminForm.UserGroupGridDragOver(Sender, Source: TObject; X,
+  Y: Integer; State: TDragState; var Accept: Boolean);
+begin
+  Accept := (TImageDragObject(Source).Control = UserGrid);
+end;
+
+procedure TAdminForm.FillGrids;
 begin
   // Fill Users:
   FillUserGrid;
 
   // Fill Groups:
-  Idx := 1;
-
-  GroupGrid.RowCount := Admin.Groups.Count + 1;
-  for Group in Admin.Groups do
-  begin
-    GroupGrid.Cells[0, Idx] := Group.Caption.Text;
-    GroupGrid.Cells[1, Idx] := '';
-  end;
+  FillGroupGrid;
 end;
 
 procedure TAdminForm.FillUserGrid;
@@ -139,12 +238,57 @@ begin
   end;
 end;
 
+procedure TAdminForm.FillGroupGrid;
+var
+  Idx: Integer;
+  Group: TEpiGroup;
+begin
+  Idx := 1;
+
+  GroupGrid.RowCount := Admin.Groups.Count + 1;
+  for Group in Admin.Groups do
+  begin
+    GroupGrid.Cells[0, Idx] := Group.Caption.Text;
+    GroupGrid.Cells[1, Idx] := '';
+
+    Inc(Idx);
+  end;
+end;
+
+function TAdminForm.UserFromGrid: TEpiUser;
+begin
+  Result := nil;
+  if UserGrid.RowCount <= 1 then Exit;
+  Result := Admin.Users[UserGrid.Row - 1];
+end;
+
 function TAdminForm.ShowUserForm(const User: TEpiUser): TModalResult;
 var
   F: TAdminUserForm;
 begin
+  if not Assigned(User) then exit;
+
   F := TAdminUserForm.Create(Self);
   F.User := User;
+  Result := F.ShowModal;
+  F.Free;
+end;
+
+function TAdminForm.GroupFromGrid: TEpiGroup;
+begin
+  Result := nil;
+  if GroupGrid.RowCount <= 1 then Exit;
+  Result := Admin.Groups[GroupGrid.Row - 1];
+end;
+
+function TAdminForm.ShowGroupForm(const Group: TEpiGroup): TModalResult;
+var
+  F: TAdminGroupForm;
+begin
+  if not Assigned(Group) then exit;
+
+  F := TAdminGroupForm.Create(Self);
+  F.Group := Group;
   Result := F.ShowModal;
   F.Free;
 end;
@@ -157,18 +301,56 @@ begin
 end;
 
 procedure TAdminForm.DeleteUserActionExecute(Sender: TObject);
+var
+  User: TEpiUser;
 begin
-  //
+  User := UserFromGrid;
+
+  if not Assigned(User) then exit;
+
+  if MessageDlg('Warning',
+       'Are you sure you want to delete the user:' + LineEnding +
+         User.FullName + ' (' + User.Login + ')',
+       mtWarning,
+       mbYesNo,
+       0,
+       mbNo
+     ) = mrYes
+  then
+    begin
+      User.Free;
+      FillUserGrid;
+    end;
 end;
 
 procedure TAdminForm.EditGroupActionExecute(Sender: TObject);
 begin
-  //
+  if ShowGroupForm(GroupFromGrid) = mrCancel then
+    Exit;
+
+  FillGroupGrid;
 end;
 
 procedure TAdminForm.DeleteGroupActionExecute(Sender: TObject);
+var
+  Group: TEpiGroup;
 begin
-  //
+  Group := GroupFromGrid;
+  if not Assigned(Group) then exit;
+
+  if MessageDlg('Warning',
+       'Are you sure you want to delete the group:' + LineEnding +
+         Group.Caption.Text,
+       mtWarning,
+       mbYesNo,
+       0,
+       mbNo
+     ) = mrYes
+  then
+    begin
+      Group.Free;
+      FillGroupGrid;
+    end;
 end;
 
 procedure TAdminForm.AddUserToGroupActionExecute(Sender: TObject);
@@ -177,11 +359,10 @@ begin
 end;
 
 procedure TAdminForm.EditUserActionExecute(Sender: TObject);
-var
-  User: TEpiUser;
 begin
-  User := Admin.Users[UserGrid.Row - 1];
-  ShowUserForm(User);
+  if ShowUserForm(UserFromGrid) = mrCancel then
+    Exit;
+
   FillGrids;
 end;
 
@@ -191,8 +372,14 @@ begin
 end;
 
 procedure TAdminForm.NewGroupActionExecute(Sender: TObject);
+var
+  Group: TEpiGroup;
 begin
-  //
+  Group := Admin.Groups.NewGroup;
+  if ShowGroupForm(Group) = mrCancel then
+    Group.Free
+  else
+    FillGroupGrid;
 end;
 
 end.
--- admin/admin_group_form.pas
+++ admin/admin_group_form.pas
@@ -15,17 +15,22 @@ type
   TAdminGroupForm = class(TForm)
     BitBtn1: TBitBtn;
     BitBtn2: TBitBtn;
-    CheckGroup2: TCheckGroup;
-    Edit3: TEdit;
+    ManageRightsChkGrp: TCheckGroup;
+    CaptionEdit: TEdit;
     Label3: TLabel;
     PageControl1: TPageControl;
     Panel2: TPanel;
     BasicSheet: TTabSheet;
     UsersSheet: TTabSheet;
+    procedure BitBtn1Click(Sender: TObject);
   private
-    { private declarations }
+    FGroup: TEpiGroup;
+    procedure FillRights;
+    procedure FormShow(Sender: TObject);
   public
     { public declarations }
+    constructor Create(TheOwner: TComponent); override;
+    property Group: TEpiGroup read FGroup write FGroup;
   end;
 
 var
@@ -34,6 +39,30 @@ var
 implementation
 
 {$R *.lfm}
+
+{ TAdminGroupForm }
+
+procedure TAdminGroupForm.BitBtn1Click(Sender: TObject);
+begin
+  Group.Caption.Text := CaptionEdit.Text;
+end;
+
+procedure TAdminGroupForm.FillRights;
+begin
+  // TODO
+end;
+
+procedure TAdminGroupForm.FormShow(Sender: TObject);
+begin
+  CaptionEdit.Text := Group.Caption.Text;
+end;
+
+constructor TAdminGroupForm.Create(TheOwner: TComponent);
+begin
+  inherited Create(TheOwner);
+
+  OnShow := @FormShow;
+end;
 
 end.
 
