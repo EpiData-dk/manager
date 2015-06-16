75f242bb80ea02afea86b8c91f794a3c249e09b9
torstenchr <github.com@bondechristiansen.dk>
2015-06-16 09:53:25 +0000

    * Fixed bug in double entry validation, that could lead to A/V due to incorrect order of adding selectiong of key fields

--- tools/validate_double_entry_frame.pas
+++ tools/validate_double_entry_frame.pas
@@ -310,12 +310,14 @@ var
 
   procedure BuildList;
   var
-    i: integer;
     L: TList;
     DF: TEpiDataFile;
     F: TEpiField;
+    DFs: TEpiDataFiles;
   begin
-    for DF in FProjectTree.Documents[0].DataFiles do
+    DFs := FProjectTree.Documents[0].Relations.GetOrderedDataFiles;
+
+    for DF in DFs do
     begin
       L := TList.Create;
       for F in DF.KeyFields do
@@ -328,6 +330,8 @@ var
           L.Add(F);
       FCompareTreeList.Add(L);
     end;
+
+    DFs.Free;
   end;
 
   procedure ClearList;
