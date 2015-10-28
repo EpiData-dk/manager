68275ffe8b0ad67167a14fec4a907545f5e9d9a5
torstenchr <github.com@bondechristiansen.dk>
2015-10-28 07:05:35 +0000

    .

--- settings/settings_statusbar.pas
+++ settings/settings_statusbar.pas
@@ -40,7 +40,9 @@ implementation
 uses
   epiv_custom_statusbar, epiv_statusbar_item_recordcount, epiv_statusbar_item_cycleno,
   epiv_statusbar_item_currentuser, epiv_statusbar_item_savetime,
-  epiv_statusbar_item_selectionnames;
+  epiv_statusbar_item_selectionnames,
+
+  CheckBoxThemed;
 
 { TSettingsStatusbar }
 
@@ -49,14 +51,44 @@ var
   L: TStringList;
   CSBClass: TEpiVCustomStatusBarItemClass;
   i: Integer;
+  CB: TCheckBoxThemed;
+  ExItem: TEpiVCustomStatusBarItem;
+  PrevPanel: TCustomPanel;
+  ItemCaption: TLabel;
 begin
   inherited Create(TheOwner);
 
   ScrollBox1.BorderStyle := bsNone;
 
+  PrevPanel := nil;
+
   for i := 0 to EpiV_GetCustomStatusBarItems.Count - 1 do
   begin
+    ExItem := TEpiVCustomStatusBarItemClass(EpiV_GetCustomStatusBarItems.Objects[i]).Create(nil);
+    ExItem.Panel.AnchorToNeighbour(akLeft, 5, Bevel2);
+    ExItem.Panel.AnchorToNeighbour(akRight, 5, Bevel3);
+    if (i = 0) then
+      ExItem.Panel.AnchorParallel(akTop, 5, ScrollBox1)
+    else
+      ExItem.Panel.AnchorToNeighbour(akTop, 5, PrevPanel);
+    ExItem.Panel.Height := 26;
+    ExItem.Panel.Parent := ScrollBox1;
+    PrevPanel := ExItem.Panel;
 
+    CB := TCheckBoxThemed.Create(Self);
+    CB.Caption := '';
+    CB.AnchorParallel(akLeft, 5, ScrollBox1);
+    CB.AnchorVerticalCenterTo(PrevPanel);
+    CB.AutoSize := true;
+    CB.Parent := ScrollBox1;
+
+    ItemCaption := TLabel.Create(Self);
+    ItemCaption.AnchorToNeighbour(akLeft, 5, Bevel1);
+    ItemCaption.AnchorToNeighbour(akRight, 5, Bevel2);
+    ItemCaption.AnchorVerticalCenterTo(PrevPanel);
+    ItemCaption.AutoSize := false;
+    ItemCaption.Parent := ScrollBox1;
+    ItemCaption.Caption := TEpiVCustomStatusBarItemClass(EpiV_GetCustomStatusBarItems.Objects[i]).Caption;
   end;
 end;
 
